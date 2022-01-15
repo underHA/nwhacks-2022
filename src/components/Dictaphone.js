4b3491d2d81cc015a705e2cb60eeee713e0d458e
Voctor <voctorbidoof@gmail.com>
2022-01-15 14:55:06 -0800

    detect dictation cutoffs for sentences

--- src/components/Dictaphone.js
+++ src/components/Dictaphone.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRef, useState, useEffect } from "react";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const Dictaphone = () => {
@@ -9,11 +10,51 @@ const Dictaphone = () => {
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
 
+  const [transcriptArray, setTranscriptArray] = useState([]);
+  const [firstTranscriptIndex, setTranscriptIndex] = useState(0);
+  const [pauseTimestamp, setPauseTimestamp] = useState(Date.now() / 1000);
+  const [sentTimestamp, setSentTimestamp] = useState(Date.now() / 1000);
+  const [relevantText, setRelevantText] = useState("");
+
+  const startListening = () => { SpeechRecognition.startListening({ continuous: true }) };
+
+  // function for sending transcript to backend.
+  // Transcript is locally defined. Currently: a string
+  const sendTranscript = (array) => { 
+    // want to show newest parts of transcript array. so, set a new index.
+    setTranscriptIndex(array.length - 1);
+    setSentTimestamp(Date.now() / 1000);
+  }
+
+  // React detects that transcript is changing
+  useEffect(() => {
+    let array = transcript.split(' ');
+
+    // compare the seconds elapsed from the latest message. if it's a significant pause (2 seconds), send all new text
+    // OR: 10 seconds has elapsed.
+    if ((Date.now() / 1000) - pauseTimestamp > 2 || (Date.now() / 1000) - sentTimestamp > 10) {
+      sendTranscript(array)
+      // TODO: check the last send time
+    }
+
+    // send is an array
+    let send = array.splice(firstTranscriptIndex, array.length);
+    // re-init the array after we finish processing
+    // TODO: might not need
+    setTranscriptArray(array);
+    
+    setRelevantText(send.join(' '));
+    console.log(send);
+
+    // TODO: add a timestamp for last sent as well
+    // save a new pauseTimestamp after every transcript update
+    setPauseTimestamp(Date.now() / 1000);
+
+  }, [transcript])
+
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
-
-  const startListening = () => { SpeechRecognition.startListening({ continuous: true }) };
 
   return (
     <div>
@@ -21,7 +62,7 @@ const Dictaphone = () => {
       <button onClick={startListening}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <p>{relevantText}</p>
     </div>
   );
 };
